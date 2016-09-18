db2f8be697aac099924aa9f880a5bb68e8e31e95
Nellouille974 <lariviere.nelly@yahoo.fr>
2016-09-18 19:58:03 +0200

    Step 3 - USER LIST PAGE

--- FAST-v1.md
+++ FAST-v1.md
@@ -2,5 +2,7 @@ All commands
 ============
 - php symfony.phar new fast-v1
 - CHMOD 777 -R var
-- bin/console generate:bundle
-- bin/console generate:entity
+- php bin/console generate:bundle
+- php bin/console generate:entity
+- php bin/console doctrine:schema:update
+- php bin/console doctrine:generate:crud
---
+++ src/NL/UserBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace NL\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullname')
+            ->add('email')
+            ->add('password')
+            ->add('birthdate', 'date')
+            ->add('createdAt', 'datetime')
+            ->add('updatedAt', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'NL\UserBundle\Entity\User'
+        ));
+    }
+}
